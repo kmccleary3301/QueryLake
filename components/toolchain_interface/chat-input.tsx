03d564aba064dddcc2909986d1a8c12895def8fe
kmccleary3301 <kmccl24@lsu.edu>
2024-04-10 15:29:05 -0500

    Removed redundant code

--- components/toolchain_interface/chat-input.tsx
+++ components/toolchain_interface/chat-input.tsx
@@ -1,9 +1,6 @@
 "use client";
-
-import { cn } from "@/lib/utils";
 import { Skeleton } from "@/registry/default/ui/skeleton";
 import { inputMapping } from "@/types/toolchain-interface";
-import { useEffect } from "react";
 import tailwindToObject from "@/hooks/tailwind-to-obj/tailwind-to-style-obj-imported";
 import { useContextAction } from "@/app/context-provider";
 
@@ -16,8 +13,6 @@ export function ChatInputSkeleton({
 }) {
   const { breakpoint } = useContextAction();
   
-	useEffect(() => {console.log("ChatInputSkeleton configuration:", configuration)}, [configuration]);
-
   return (
     <Skeleton 
       className="rounded-md h-10 border-dashed border-[2px] border-primary/50 flex flex-col justify-center"
