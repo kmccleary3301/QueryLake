590e1e6474bb771e4d8ec2053812f02d5c8e34a5
kmccleary3301 <kmccl24@lsu.edu>
2025-02-10 13:11:30 -0600

    Small fix to new wrapping CSS

--- app/docs/layout.tsx
+++ app/docs/layout.tsx
@@ -11,7 +11,7 @@ export default function DocsLayout({
       <div className="w-full h-[calc(100vh)]">
         <ScrollArea className="w-full h-screen @container/docs">
           <div className="flex flex-row justify-center">
-            <div className="w-[min(90cqw,70vw)] lg:w-[min(90cqw,55vw)] xl:w-[min(90cqw,45vw)] sm:w-[min(90cqw,80vw)]">
+            <div className="md:w-[min(90cqw,65vw)] lg:w-[min(90cqw,55vw)] xl:w-[min(90cqw,45vw)] w-[min(90cqw,80vw)]">
               <div className="px-[3rem]">
 								{children}
 							</div>
